f0f0a89301dba6a9049cbbf95ff65c33e288c83b
DJ_Yazid <y.djaoui@rubika-edu.com>
2025-01-09 00:29:23 +0100

    Communauté patrol

---
+++ WorkShopRTS/Assets/CommunityPatrol.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Rendering;
+
+
+public class CommunityPatrol : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public List<GameObject> patrolPoints = new List<GameObject>();
+    public List<GameObject> unitChild = new List<GameObject>();
+    private int currentPatrolIndex = 0;
+    private float distance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for(int i = 0; i < unitChild.Count; i++)
+        {
+            unitChild[i].GetComponent<UnitCommunityMovement>().unitPatrolPoint = patrolPoints[0].transform.position;
+            unitChild[i].GetComponent<UnitCommunityMovement>().isPatroling = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckChildren()
+    {
+        float countValid = 0;
+        for(int i = 0; i < unitChild.Count; i++)
+        {
+            if(unitChild[i].GetComponent<UnitCommunityMovement>().isArrived == true)
+            {
+                countValid ++;
+            }
+            else
+            {
+                countValid = 0;
+                return;
+            }
+        }
+
+        if(countValid == unitChild.Count)
+        {
+            currentPatrolIndex ++;
+            if(currentPatrolIndex > patrolPoints.Count - 1)
+            {
+                currentPatrolIndex = 0;
+            }
+
+            for(int i = 0; i < unitChild.Count; i++)
+            {
+                unitChild[i].GetComponent<UnitCommunityMovement>().newPatrolPoint(patrolPoints[currentPatrolIndex]);
+            }
+        }
+    }
+
+     private IEnumerator WaitAndLeave(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        currentPatrolIndex += 1;
+        if(currentPatrolIndex > patrolPoints.Count)
+        {
+            currentPatrolIndex = 0;
+        }
+    }
+}
---
+++ WorkShopRTS/Assets/UnitCommunityMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitCommunityMovement : MonoBehaviour
+{
+    [SerializeField]
+    private CommunityPatrol communityPatrol;
+    NavMeshAgent agent;
+
+    public bool isPatroling;
+    private float distance;
+    public Vector3 unitPatrolPoint;
+    public bool isArrived;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        communityPatrol = transform.parent.parent.GetComponent<CommunityPatrol>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(unitPatrolPoint,transform.position);
+        if(distance <= 2)
+            {
+                if(isArrived == false)
+                {
+                    isArrived = true;
+                    communityPatrol.CheckChildren();
+                }
+            }
+
+        if(isPatroling)
+        {
+            agent.SetDestination(unitPatrolPoint);
+        }
+    }
+
+    public void newPatrolPoint(GameObject parentPatrolPoint)
+    {
+        unitPatrolPoint = parentPatrolPoint.transform.position;
+        isArrived = false;
+    }
+}
