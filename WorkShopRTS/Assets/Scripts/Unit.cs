2e1ed903b2fce05171d57b4e9b44335e22f12347
DJ_Yazid <y.djaoui@rubika-edu.com>
2025-01-08 03:30:58 +0100

    Selection des unités ( pas tout à fait fini )

--- WorkShopRTS/Assets/Scripts/Unit.cs
+++ WorkShopRTS/Assets/Scripts/Unit.cs
@@ -2,10 +2,22 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [Header("Units Values")]
+    public string UnitName;
+    public int level;
+    public float health;
+    public float damage;
+    public float moveSpeed;
+    public float thirstValue;
     public UnitInventory inventory;
     void Start()
     {
-        
+        UnitSelectionManager.Instance.allUnitsList.Add(gameObject);
+    }
+
+    private void OnDestroy() 
+    {
+        UnitSelectionManager.Instance.allUnitsList.Remove(gameObject);
     }
 
     // Update is called once per frame
---
+++ WorkShopRTS/Assets/UnitSelectionManager.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+
+public class UnitSelectionManager : MonoBehaviour
+{
+
+    public static UnitSelectionManager Instance { get; set;}
+
+    [Header("List")]
+    public List<GameObject> allUnitsList = new List<GameObject>();
+    public List<GameObject> unitsSelected = new List<GameObject>();
+
+    [Header("Click Variable")]
+    public LayerMask clickable;
+    public LayerMask ground;
+    public GameObject groundMarker;
+
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            // If we are hitting a clickable object
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity, clickable))
+            {
+                if(Input.GetKey(KeyCode.LeftShift))
+                {
+                    MultiSelect(hit.collider.gameObject);
+                }
+                else
+                {
+                    SelectByClicking(hit.collider.gameObject);
+                }
+            }
+            // If we are NOT hitting a clickable object
+            else
+            {
+                if(Input.GetKey(KeyCode.LeftShift) == false)
+                {
+                    DeselectAll();
+                }
+            }
+        }
+
+
+         if(Input.GetMouseButtonDown(1) && unitsSelected.Count > 0)
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            // If we are hitting a clickable object
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
+            {
+                groundMarker.transform.position = hit.point;
+                groundMarker.SetActive(false);
+                groundMarker.SetActive(true);
+            }
+        }
+    }
+
+    private void SelectByClicking(GameObject unit)
+    {
+        DeselectAll();
+        unitsSelected.Add(unit);
+
+        TriggerSelectionIndicator(unit, true);
+        EnableUnitMovement(unit,true);
+    }
+    private void DeselectAll()
+    {
+        foreach(var unit in unitsSelected)
+        {
+            EnableUnitMovement(unit, false);
+            TriggerSelectionIndicator(unit, false);
+        }
+        
+        groundMarker.SetActive(false);
+        unitsSelected.Clear();
+    }
+    private void EnableUnitMovement(GameObject unit, bool shouldMove)
+    {
+        
+    }
+    private void MultiSelect(GameObject unit)
+    {
+        if(unitsSelected.Contains(unit) == false)
+        {
+            unitsSelected.Add(unit);
+            TriggerSelectionIndicator(unit, true);
+            EnableUnitMovement(unit, true);
+        }
+        else
+        {
+            EnableUnitMovement(unit, false);
+            TriggerSelectionIndicator(unit, false);
+            unitsSelected.Remove(unit);
+        }
+    }
+
+    private void TriggerSelectionIndicator(GameObject unit, bool isVisible)
+    {
+        unit.transform.GetChild(0).gameObject.SetActive(isVisible);
+    }
+}
