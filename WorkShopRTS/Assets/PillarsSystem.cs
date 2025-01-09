06e737b3544e1e0fb7dad2dfbaef0b869b17eac4
DJ_Yazid <y.djaoui@rubika-edu.com>
2025-01-09 11:20:18 +0100

    Monstre détruit une fois les pilliers reconstruit

--- WorkShopRTS/Assets/PillarsSystem.cs
+++ WorkShopRTS/Assets/PillarsSystem.cs
@@ -9,6 +9,7 @@ public class PillarsSystem : MonoBehaviour
     public GameObject currentPillar;
     public Mesh repairedMesh;
     public GameObject restorePillarsPanel;
+    public GameObject Monster;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,10 +28,28 @@ public class PillarsSystem : MonoBehaviour
         currentPillar.GetComponent<Pillar>().isRepaired = true;
         currentPillar.GetComponent<MeshFilter>().sharedMesh = repairedMesh;
         restorePillarsPanel.SetActive(false);
+        AffectMonster();
     }
 
     public void ClosePanel()
     {
         restorePillarsPanel.SetActive(false);
+    }
+
+    public void AffectMonster()
+    {
+        int currentRepairedPillard = 0;
+        for (int i = 0; i < pillarsList.Count; i++)
+        {
+            if(pillarsList[i].GetComponent<Pillar>().isRepaired)
+            {
+                currentRepairedPillard ++;
+            }
+            if(currentRepairedPillard == 3)
+            {
+                Destroy(Monster);
+            }
+        }
+        Monster.transform.localScale -= new Vector3(2,0,2);
     }
 }
