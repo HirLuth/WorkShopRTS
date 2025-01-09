8117c227fe95736e0a5eb3005c46cb22049e9c6c
DJ_Yazid <y.djaoui@rubika-edu.com>
2025-01-09 04:30:23 +0100

    Je suis un crack

--- WorkShopRTS/Assets/FogOfWarScript.cs
+++ WorkShopRTS/Assets/FogOfWarScript.cs
@@ -21,13 +21,76 @@ public class FogOfWarScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		Ray r = new Ray(m_player.position + new Vector3(0,10,0), new Vector3(0,-10,0));
+	void Update () 
+    {
+        // Raycasts
+		Ray rCenter = new Ray(m_player.position + new Vector3(0,10,0), new Vector3(0,-10,0));
+        Ray rRight = new Ray(m_player.position + new Vector3(0,10,0), new Vector3(-10,-10,0));
+        Ray rLeft = new Ray(m_player.position + new Vector3(0,10,0), new Vector3(10,-10,0));
+        Ray rUp = new Ray(m_player.position + new Vector3(0,10,0), new Vector3(0,-10,10));
+        Ray rDown = new Ray(m_player.position + new Vector3(0,10,0), new Vector3(0,-10,-10));
 		RaycastHit hit;
-        Debug.DrawRay(m_player.position + new Vector3(0,10,0), new Vector3(0,-10,0), Color.red);
-		if (Physics.Raycast(r, out hit, 1000, m_fogLayer, QueryTriggerInteraction.Collide)) 
+        Debug.DrawRay(m_player.position + new Vector3(0,10,0), new Vector3(0,-10,10), Color.red);
+		
+        // Raycast centre
+        if (Physics.Raycast(rCenter, out hit, 1000, m_fogLayer, QueryTriggerInteraction.Collide)) 
         {
-            Debug.Log("Detect");
+			for (int i=0; i< m_vertices.Length; i++) {
+				Vector3 v = m_fogOfWarPlane.transform.TransformPoint(m_vertices[i]);
+				float dist = Vector3.SqrMagnitude(v - hit.point);
+				if (dist < m_radiusSqr) {
+					float alpha = Mathf.Min(m_colors[i].a, dist/m_radiusSqr);
+					m_colors[i].a = alpha;
+				}
+			}
+			UpdateColor();
+		}
+
+        // Raycast centre
+        if (Physics.Raycast(rRight, out hit, 1000, m_fogLayer, QueryTriggerInteraction.Collide)) 
+        {
+			for (int i=0; i< m_vertices.Length; i++) {
+				Vector3 v = m_fogOfWarPlane.transform.TransformPoint(m_vertices[i]);
+				float dist = Vector3.SqrMagnitude(v - hit.point);
+				if (dist < m_radiusSqr) {
+					float alpha = Mathf.Min(m_colors[i].a, dist/m_radiusSqr);
+					m_colors[i].a = alpha;
+				}
+			}
+			UpdateColor();
+		}
+        
+        // Raycast gauche
+        if (Physics.Raycast(rLeft, out hit, 1000, m_fogLayer, QueryTriggerInteraction.Collide)) 
+        {
+			for (int i=0; i< m_vertices.Length; i++) {
+				Vector3 v = m_fogOfWarPlane.transform.TransformPoint(m_vertices[i]);
+				float dist = Vector3.SqrMagnitude(v - hit.point);
+				if (dist < m_radiusSqr) {
+					float alpha = Mathf.Min(m_colors[i].a, dist/m_radiusSqr);
+					m_colors[i].a = alpha;
+				}
+			}
+			UpdateColor();
+		}
+        
+        // Raycast Haut
+        if (Physics.Raycast(rUp, out hit, 1000, m_fogLayer, QueryTriggerInteraction.Collide)) 
+        {
+			for (int i=0; i< m_vertices.Length; i++) {
+				Vector3 v = m_fogOfWarPlane.transform.TransformPoint(m_vertices[i]);
+				float dist = Vector3.SqrMagnitude(v - hit.point);
+				if (dist < m_radiusSqr) {
+					float alpha = Mathf.Min(m_colors[i].a, dist/m_radiusSqr);
+					m_colors[i].a = alpha;
+				}
+			}
+			UpdateColor();
+		}
+       
+       // Raycast Bas
+        if (Physics.Raycast(rDown, out hit, 1000, m_fogLayer, QueryTriggerInteraction.Collide)) 
+        {
 			for (int i=0; i< m_vertices.Length; i++) {
 				Vector3 v = m_fogOfWarPlane.transform.TransformPoint(m_vertices[i]);
 				float dist = Vector3.SqrMagnitude(v - hit.point);
