36139b6a384b2ed4b4366e192a5f2fefc467cf6a
DJ_Yazid <y.djaoui@rubika-edu.com>
2025-01-10 04:00:51 +0100

    Niveau de beauté = God of War 2

--- WorkShopRTS/Assets/DayAndNightLogic.cs
+++ WorkShopRTS/Assets/DayAndNightLogic.cs
@@ -8,6 +8,7 @@ public class DayAndNightLogic : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        lightingManager = GameObject.Find("LightingManager").GetComponent<LightingManager>();
         lightingManager.ressourcesList.Add(gameObject);
     }
 
