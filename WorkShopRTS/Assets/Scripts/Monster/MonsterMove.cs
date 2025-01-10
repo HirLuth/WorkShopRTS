2170745b1f9a981612ea036914f7caa6b646b17a
DJ_Yazid <y.djaoui@rubika-edu.com>
2025-01-10 12:10:31 +0100

    Monster fix

--- WorkShopRTS/Assets/Scripts/Monster/MonsterMove.cs
+++ WorkShopRTS/Assets/Scripts/Monster/MonsterMove.cs
@@ -18,7 +18,8 @@ public class MonsterMove : MonoBehaviour
     private float percentagePlayerTarget = 0f;
     private float percentageRandomTarget = 100f;
 
-    private bool isChasing;
+    [HideInInspector]
+    public bool isChasing;
     private GameObject currentTarget;
 
     void Start()
@@ -48,6 +49,7 @@ public class MonsterMove : MonoBehaviour
             Vector3 goalPos = carPlayer.transform.position;
             agent.SetDestination(goalPos);
             currentGoal = goalPos;
+            percentagePlayerTarget = 0;
         }
         else
         {
